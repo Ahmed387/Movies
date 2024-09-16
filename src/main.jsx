8e93e82df0ca92d7ed885dd1b8695a1abe3e6e22
Daniels <www201820192020@gmail.com>
2024-09-16 09:06:52 +0200

    modify commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from "./App.jsx";
-
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./Redux/store.js";
+import { store } from "./Redux/Store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
